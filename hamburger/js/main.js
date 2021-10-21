0f7445b9777aadf4b5fe4542b249e03a4472f07e
ryowakitazawa82 <ryowa.kitazawa@gmail.com>
2021-10-22 00:43:03 +0900

    renew

--- hamburger/js/main.js
+++ hamburger/js/main.js
@@ -19,3 +19,25 @@
         // ばつ印を非表示に
     });
 }
+
+// 'use strict';
+
+// {
+//     const open = document.getElementById('open');
+//     const overlay = document.querySelector('.overlay');
+//     const close = document.getElementById('close');
+
+//     open.addEventListener('click',() => {
+//         overlay.classList.add('show');
+//         // オーバーレイを表示させる
+//         open.classList.add('hide');
+//         // ハンバーガーメニューを非表示にする
+//     });
+
+//     close.addEventListener('click',() => {
+//         overlay.classList.remove('show');
+//         // オーバーレイを非表示にさせる
+//         open.classList.remove('hide');
+//         // ばつ印を非表示に
+//     });
+// }
